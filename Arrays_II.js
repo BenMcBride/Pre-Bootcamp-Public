472e69c0488f57b7493425e2b3f3d4eb4e49ac27
Benjamin McBride <96863998+BenMcBride@users.noreply.github.com>
2023-03-08 11:10:34 -0800

    Add files via upload

--- Arrays_II.js
+++ Arrays_II.js
@@ -1,14 +1,31 @@
-function optimalLocation(coord){
-  var avgx = 0
-  var avgy = 0
-  for(var i = 0; i < coord.length; i++){
+function optimalLocation(coord) {
+  var avgx = 0;
+  var avgy = 0;
+  for (var i = 0; i < coord.length; i++) {
     avgx = avgx + coord[i][0];
     avgy = avgy + coord[i][1];
-  };
+  }
   avgx = avgx / coord.length;
   avgy = avgy / coord.length;
-  coord = [Math.round(avgx),Math.round(avgy)];
-  console.log(coord)
-  return coord
+  coord = [Math.round(avgx), Math.round(avgy)];
+  console.log(coord);
+  return coord;
 }
-optimalLocation([[2,5],[10,10],[-5,-18],[-10,5]])
+optimalLocation([
+  [2, 5],
+  [10, 10],
+  [-5, -18],
+  [-10, 5],
+]);
+optimalLocation([
+  [1, 25],
+  [-53, 2],
+  [5, 2],
+  [14, -30],
+  [-12, 20],
+  [-58, 12],
+]);
+optimalLocation([
+  [1, 1],
+  [1, 1],
+]);
