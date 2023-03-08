fd899cea18f2984d4b71e4ee6f79ec1619ece00e
Benjamin McBride <96863998+BenMcBride@users.noreply.github.com>
2023-03-08 11:35:24 -0800

    Add files via upload

---
+++ Conditionals.js
@@ -0,0 +1,24 @@
+function reward(day, time) {
+  if (time < 10 && time >= 0) {
+    return "latte";
+  } else if (time < 15 && time >= 10) {
+    return "hot chocolate";
+  } else if (time < 18 && time >= 15) {
+    if (time % 2 === 0) {
+      var even = ["ice cream", "cookies", "candy"];
+      return even[Math.floor(Math.random() * 3)];
+    } else {
+      var odd = ["hot chocolate", "tea", "cake"];
+      return odd[Math.floor(Math.random() * 3)];
+    }
+  } else if (time < 22 && time >= 18) {
+    if (day === "Wednesday" || day === "wednesday" || day === "wed") {
+      return "Strawberry ice cream";
+    } else {
+      return "Vanilla ice cream";
+    }
+  } else if (time < 24 && time >= 22) {
+    return "Sleep!";
+  }
+}
+console.log(reward("wtues", 18));
